59e95322bc32531b5bc37ba7ebe2dcbdb48360c8
Shi Wei Zheng <shiz3150@hstat.org>
2019-01-08 17:30:06 +0000

    change word choice

--- oo-final-project.rb
+++ oo-final-project.rb
@@ -8,60 +8,60 @@ class Tool
         @current_time = 0
     end
     
-    def damage(material_damaged)
-        until @current_time == @time || material_damaged.material_health == 0 || @tool_health == 0
-            material_damaged.material_health -= 5
+    def damage(item_damaged)
+        until @current_time == @time || item_damaged.item_health == 0 || @tool_health == 0
+            item_damaged.item_health -= 5
             @tool_health -= 10
             @current_time += 1
         end
     end
 end
 
-class Material
-    attr_accessor :material_health
-    def initialize(material_name, material_health)
-        @material_name = material_name
-        @material_health = material_health
+class Item
+    attr_accessor :item_health
+    def initialize(item_name, item_health)
+        @item_name = item_name
+        @item_health = item_health
     end
     
     def broken(tool_condition)
-        if @material_health == 0 && tool_condition.tool_health == 0
-            puts "Both #{@material_name} and #{tool_condition.tool_name} break at the same time!"
-        elsif @material_health == 0
-            puts "The #{@material_name} breaks first!"
+        if @item_health == 0 && tool_condition.tool_health == 0
+            puts "Both #{@item_name} and #{tool_condition.tool_name} break at the same time!"
+        elsif @item_health == 0
+            puts "The #{@item_name} breaks first!"
         elsif tool_condition.tool_health == 0
             puts "Your #{tool_condition.tool_name} breaks first!"
         else
-            puts "Nothing breaks. Your #{tool_condition.tool_name}'s health is now #{tool_condition.tool_health}. The #{@material_name}'s health is now #{@material_health}"
+            puts "Nothing breaks. \nYour #{tool_condition.tool_name}'s health is now #{tool_condition.tool_health}. \nThe #{@item_name}'s health is now #{@item_health}"
         end
     end
 end
 
 def start
-    puts "This is a game where you create a tool and an object you want to destroy. You will choose how many times you want to hit the object to see which one breaks first, none of them breaks, or both breaks at the same time. Your tool will have 100 health and you get to choose the heath of the object."
+    puts "This is a game where you create a tool and an item you want to destroy. You will choose how many times you want to hit the item to see which one breaks first, none of them breaks, or both breaks at the same time. Your tool will have 100 health and you get to choose the heath of the item."
     puts "What tool will you use?"
     name_of_tool = gets.chomp
-    puts "What object will you hit?"
-    name_of_object = gets.chomp
-    puts "What is the health of the #{name_of_object}?"
-    health_of_object = gets.chomp.to_i
-    puts "How many times will you like to hit?"
+    puts "What is the item you'll hit?"
+    name_of_item = gets.chomp
+    puts "What is the health of the #{name_of_item}?"
+    health_of_item = gets.chomp.to_i
+    puts "How many times will you hit?"
     number_of_hit = gets.chomp.to_i
-    tools = Tool.new(name_of_tool, number_of_hit)
-    materials = Material.new(name_of_object, health_of_object)
-    tools.damage(materials)
-    materials.broken(tools)
+    tool = Tool.new(name_of_tool, number_of_hit)
+    item = Item.new(name_of_item, health_of_item)
+    tool.damage(item)
+    item.broken(tool)
 end
 
 start
 loop do
 	puts "Do you want to keep playing?"
-	answer = gets.chomp
+	answer = gets.chomp.downcase
 	if answer == "yes"
 		start
 	elsif answer == "no"
 		break
 	else
-		redo 
+		redo
 	end
 end
